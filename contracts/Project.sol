7a0cbcd99bc98741c51b57fb7c079f36c7eaa279
Makoto Inoue <inouemak@googlemail.com>
2016-12-14 18:06:03 +0000

    Track project detail

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,37 +1,36 @@
 pragma solidity ^0.4.2;
 
 contract Project {
+  Detail public detail;
 
   struct Detail{
     address owner;
+    string title;
+    string description;
     uint targetAmount;
     uint deadline;
   }
 
-  function Project() {
-    // constructor
+  struct Contributor{
+    address contributorAddress;
+    uint amount;
   }
 
-  /*
-    This is the function called when the FundingHub receives a contribution.
-    The function must keep track of the contributor and the individual amount contributed.
-    If the contribution was sent after the deadline of the project passed,
-    or the full amount has been reached,
-    the function must return the value to the originator of the transaction and
-    call one of two functions.
-    If the full funding amount has been reached, the function must call payout.
-    If the deadline has passed without the funding goal being reached, the function must call refund.
-  */
-  function fund();
+  /*function Project(string _title, string _description, uint _targetAmount, uint _deadline) {*/
+  function Project() {
+    detail = Detail(msg.sender, 'title', 'desription', 1,1);
+  }
+
+  function fund(){}
 
   /*
     This is the function that sends all funds received in the contract to the owner of the project.
   */
-  function payout();
+  function payout(){}
 
   /*
     This function sends all individual contributions back to the respective contributor,
     or lets all contributors retrieve their contributions.
   */
-  function refund();
+  function refund(){}
 }
--- test/project.js
+++ test/project.js
@@ -1,7 +1,55 @@
 contract('Project', function(accounts) {
-  it("should assert true", function(done) {
-    var project = Project.deployed();
-    assert.isTrue(true);
-    done();
-  });
+
+  /*
+    This is the function called when the FundingHub receives a contribution.
+    The function must keep track of the contributor and the individual amount contributed.
+    If the contribution was sent after the deadline of the project passed,
+    or the full amount has been reached,
+    the function must return the value to the originator of the transaction and
+    call one of two functions.
+    If the full funding amount has been reached, the function must call payout.
+    If the deadline has passed without the funding goal being reached, the function must call refund.
+  */
+  describe('constructor', function(){
+    it.only('creates new project', function(done){
+      var project;
+      var owner = accounts[0];
+      var title = 'My pet project';
+      var description = 'Very nice project';
+      var targetAmount = web3.toWei(100);
+      var a_day = 1 * 60 * 60 * 24 // 1 day
+      var deadline = a_day * 8;
+      Project.new({from:owner})
+      .then(function(_project) {
+        project = _project;
+        return project.detail.call()
+      })
+      .then(function(detail) {
+        assert.equal(detail[0], owner);
+        //  {
+        //   address:owner,
+        //   title:title,
+        //   description:description,
+        //   targetAmount:targetAmount,
+        //   deadline:deadline
+        // })
+      })
+      .then(done);
+    })
+  })
+
+  describe('fund', function(){
+    it('keeps track of contributor and contribution amount', function(done){
+    })
+  })
+
+  describe('payout', function(){
+    it('payouts if the full funding amount has been reached')
+  })
+
+  describe('refund', function(){
+    it("refunds the fund to the contributor if funding goal has not been reached", function(done) {
+
+    });
+  })
 });
