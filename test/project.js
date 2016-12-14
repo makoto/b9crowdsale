7be1a89f9d35825ebb43592ee73c900c2e9ae11c
Makoto Inoue <inouemak@googlemail.com>
2016-12-14 18:43:54 +0000

    Add spec for next feature

--- test/project.js
+++ test/project.js
@@ -11,13 +11,16 @@ contract('Project', function(accounts) {
     If the deadline has passed without the funding goal being reached, the function must call refund.
   */
   describe('constructor', function(){
-    it.only('creates new project', function(done){
-      var project;
-      var owner = accounts[0];
-      var title = 'My pet project';
-      var targetAmount = web3.toWei(100);
-      var a_day = 1 * 60 * 60 * 24 // 1 day
-      var deadline = a_day * 8;
+    var project;
+    var owner = accounts[0];
+    var backer = accounts[1];
+    var title = 'My pet project';
+    var targetAmount = web3.toWei(10);
+    var contribution = web3.toWei(1);
+    var a_day = 1 * 60 * 60 * 24 // 1 day
+    var deadline = a_day * 8;
+
+    it('creates new project', function(done){
       Project.new(title, targetAmount, deadline, {from:owner})
       .then(function(_project) {
         project = _project;
@@ -31,10 +34,27 @@ contract('Project', function(accounts) {
       })
       .then(done);
     })
+    it('does not allow minus deadline')
+    it('does not allow minus target')
+    it('allows duplicate title')  // just for documentation purpose
+    it('allows empty title')      // just for documentation purpose
   })
 
   describe('fund', function(){
     it('keeps track of contributor and contribution amount', function(done){
+      Project.new(title, targetAmount, deadline, {from:owner})
+      .then(function(_project) {
+        project = _project;
+        return project.fund.sendTransaction({from:backer, amount:contribution})
+      })
+      .then(function() {
+        return project.contributor.call(backer)
+      })
+      .then(function(contributor) {
+        assert.equal(contributor[0], backer);
+        assert.equal(contributor[0], contribution);
+      })
+      .then(done);
     })
   })
 
