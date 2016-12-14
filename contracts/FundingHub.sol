5d9931179dd9c9a31695d643a206f6f839cab511
Makoto Inoue <inouemak@googlemail.com>
2016-12-14 16:28:08 +0000

    Add skeleton methods

--- contracts/FundingHub.sol
+++ contracts/FundingHub.sol
@@ -1,5 +1,21 @@
+pragma solidity ^0.4.2;
+
 contract FundingHub {
   function FundingHub() {
     // constructor
   }
+
+  /*
+    This function should allow a user to add a new project to the FundingHub.
+    The function should deploy a new Project contract and keep track of its address.
+    The createProject() function should accept all constructor values that the Project contract requires.
+  */
+  function createProject();
+
+  /*
+    This function allows users to contribute to a Project identified by its address.
+    contribute calls the fund() function in the individual Project contract
+    and passes on all value attached to the function call.
+  */
+  function contribute();
 }
--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,5 +1,30 @@
+pragma solidity ^0.4.2;
+
 contract Project {
   function Project() {
     // constructor
   }
+
+  /*
+    This is the function called when the FundingHub receives a contribution.
+    The function must keep track of the contributor and the individual amount contributed.
+    If the contribution was sent after the deadline of the project passed,
+    or the full amount has been reached,
+    the function must return the value to the originator of the transaction and
+    call one of two functions.
+    If the full funding amount has been reached, the function must call payout.
+    If the deadline has passed without the funding goal being reached, the function must call refund.
+  */
+  function fund();
+
+  /*
+    This is the function that sends all funds received in the contract to the owner of the project.
+  */
+  function payout();
+
+  /*
+    This function sends all individual contributions back to the respective contributor,
+    or lets all contributors retrieve their contributions.
+  */
+  function refund();
 }
