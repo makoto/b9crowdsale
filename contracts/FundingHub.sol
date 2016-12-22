108e209c349dd6ecebc62d966598dffb91252919
Makoto Inoue <inouemak@googlemail.com>
2016-12-22 09:23:09 +0900

    Implement createProject

--- contracts/FundingHub.sol
+++ contracts/FundingHub.sol
@@ -1,6 +1,9 @@
 pragma solidity ^0.4.2;
+import './Project.sol';
 
 contract FundingHub {
+  address[] projects;
+
   function FundingHub() {
     // constructor
   }
@@ -10,12 +13,21 @@ contract FundingHub {
     The function should deploy a new Project contract and keep track of its address.
     The createProject() function should accept all constructor values that the Project contract requires.
   */
-  function createProject();
+  function createProject(bytes32 _title, uint _targetAmount, uint _deadline) public{
+    var projectAddress = new Project(_title, _targetAmount, _deadline);
+    projects.push(projectAddress);
+  }
 
   /*
     This function allows users to contribute to a Project identified by its address.
     contribute calls the fund() function in the individual Project contract
     and passes on all value attached to the function call.
   */
-  function contribute();
+  function contribute() public{
+
+  }
+
+  function numOfProjects() public constant returns (uint){
+    return projects.length;
+  }
 }
--- test/funding_hub.js
+++ test/funding_hub.js
@@ -1,7 +1,48 @@
+'use strict';
+
 contract('FundingHub', function(accounts) {
-  it("should assert true", function(done) {
-    var funding_hub = FundingHub.deployed();
-    assert.isTrue(true);
-    done();
-  });
+  var hub;
+  var owner = accounts[0];
+  var backer = accounts[1];
+  var another_backer = accounts[2];
+  var title = 'My pet project';
+  var targetAmount = parseInt(web3.toWei(10));
+  var contribution = parseInt(web3.toWei(1));
+  var a_day = 1 * 60 * 60 * 24 // 1 day
+  var deadline = a_day * 8;
+
+  describe('constructor', function(){
+    // FundingHub.deployed()
+  })
+
+  /*
+    This function should allow a user to add a new project to the FundingHub.
+    The function should deploy a new Project contract and keep track of its address.
+    The createProject() function should accept all constructor values that the Project contract requires.
+  */
+  describe('createProject', function(){
+    it.only('registers new project', function(done){
+      FundingHub.new()
+        .then(function(_hub) {
+          hub = _hub;
+          return hub.createProject(title, targetAmount, deadline, {from:owner});
+        })
+        .then(function() {
+          return hub.numOfProjects.call();
+        })
+        .then(function(num) {
+          assert.strictEqual(num.toNumber(), 1);
+        })
+        .then(done);
+    })
+  })
+
+  /*
+    This function allows users to contribute to a Project identified by its address.
+    contribute calls the fund() function in the individual Project contract
+    and passes on all value attached to the function call.
+  */
+  describe('contribute', function(){
+
+  })
 });
